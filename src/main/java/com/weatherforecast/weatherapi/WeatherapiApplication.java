08c006e82b4dac25d42a6231ccf4083dba85cd91
Maciej <maciejsikorski57@gmail.com>
2024-06-12 19:34:51 +0200

    Initial commit

--- src/main/java/com/weatherforecast/weatherapi/WeatherapiApplication.java
+++ src/main/java/com/weatherforecast/weatherapi/WeatherapiApplication.java
@@ -1,5 +1,6 @@
 package com.weatherforecast.weatherapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,7 +8,12 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class WeatherapiApplication {
+
+
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		System.setProperty("API_KEY", dotenv.get("API_KEY"));
 		SpringApplication.run(WeatherapiApplication.class, args);
 	}
 }
